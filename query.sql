9f6f8f49731f3a70ad5d9ad0cf972e7b6a4565f7
tonussi <lptonussi@gmail.com>
2014-11-25 18:22:38 -0200

    Adiciona test de select simples

---
+++ query.sql
@@ -0,0 +1,19 @@
+-- test
+
+select * from orcamento; -- (id, valor, data)
+select * from material; -- (id, descricao)
+select * from orcamentosmateriais; -- (id, quantidade, preco, #codOrc, #codMat)
+select * from homologado; -- (id, #codAss, #codAloc, #codMat)
+select * from local; -- (id, andar, setor, numero)
+select * from alocacao; -- (id, dataalocacao, #codLocal, #codHom)
+select * from assinatura; -- (id, dataassinatura, #codFun, #codHomo)
+select * from funcionario; -- (id, nome, idade, salario, dataegresso, dataingresso)
+select * from projeto; -- (id, brevedescricao, datainicio)
+select * from participacaoprojeto; -- (id, datainicio, datafim, #codFun, #codProj)
+select * from tarefa; -- (id, #codProj, brevedescricao, estado, prioridade,
+tempogasto)
+select * from coordenador; -- (id, designacao, #codFun)
+select * from coordenadorprojeto; -- (#codCoord, #codProj)
+select * from programador; -- (id, #codFun)
+select * from linguagem; -- (id, nome)
+select * from programadorlinguagem; -- (id, dominio, #codProg, #codLing)
