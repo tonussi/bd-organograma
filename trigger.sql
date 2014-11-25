8ff4ba71410da3fb5b559aeb87e22ae755d0c26c
tonussi <lptonussi@gmail.com>
2014-11-25 18:15:44 -0200

    Add ajustes nos nomes

--- trigger.sql
+++ trigger.sql
@@ -1,33 +1,33 @@
 -- quando alguma tarefa expira o tempo dela ser executada.
 
-create function tarefa_expirou()
-returns trigger as $body$
+create or replace function tarefa_expirou()
+returns trigger as $tarefa_expirou$
   begin
     if new.expirationdate > current_timestamp then
       return new;
     else
       return null;
+    end if;
   end;
-$body$
-language 'plpgsql';
+$tarefa_expirou$ language plpgsql;
 
-create trigger verificar_data_expiracao before insert or update on product
+create or replace trigger verificar_data_expiracao before insert or update on product
   for each row execute procedure tarefa_expirou();
 
-create function funcionario_salario() returns trigger as $funcionario_salario$
+create or replace function funcionario_salario() returns trigger as $funcionario_salario$
   begin
 
-    if new.nome is null then
-        raise exception 'Nome não pode ser nulo';
+   if new.nome is null then
+        raise exception 'nome não pode ser nulo';
     end if;
 
     if new.salario is null then
-        raise exception 'Funcionário % não pode ser nulo', new.nome;
+        raise exception 'funcionário % não pode ser nulo', new.nome;
     end if;
 
     -- who works for us when she must pay for it?
     if new.salario < 0 then
-        raise exception 'Funcionário % não pode ter salário negativo', new.nome;
+        raise exception 'funcionário % não pode ter salário negativo', new.nome;
     end if;
 
     new.last_date := current_timestamp;
@@ -38,32 +38,34 @@ create function funcionario_salario() returns trigger as $funcionario_salario$
   end;
 $funcionario_salario$ language plpgsql;
 
-create trigger funcionario_salario before insert or update on funcionario
+create or replace trigger funcionario_salario before insert or update on funcionario
   for each row execute procedure funcionario_salario();
 
-create function funcionario_salario() returns trigger as $funcionario_salario$
+create or replace function tarefas8_80() returns trigger as $funcionario_salario$
+  declare diferenca8_80 integer; set select age('2013-02-28 11:01:28'::timestamp, '2011-12-31 11:00'::timestamp);
+
   begin
 
     if new.nome is null then
-        raise exception 'Nome não pode ser nulo';
+        raise exception 'nome não pode ser nulo';
     end if;
 
     if new.salario is null then
-        raise exception 'Funcionário % não pode ser nulo', new.nome;
+        raise exception 'funcionário % não pode ser nulo', new.nome;
     end if;
 
     -- who works for us when she must pay for it?
-    if new.salario < 0 then
-        raise exception 'Funcionário % não pode ter salário negativo', new.nome;
+    if new.tempogasto > new.tempoestimado then
+        raise exception 'funcionário % não pode ter salário negativo', new.nome;
     end if;
 
-    new.last_date := current_timestamp;
-    new.last_user := current_user;
+    -- new.last_date := current_timestamp;
+    -- new.last_user := current_user;
 
     return new;
 
   end;
 $funcionario_salario$ language plpgsql;
 
-create trigger funcionario_salario before insert or update on funcionario
-  for each row execute procedure funcionario_salario();
+create or replace trigger tarefas8_80 before insert or update on funcionario
+  for each row execute procedure tarefas8_80();
