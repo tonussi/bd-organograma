2284ca8d8853775663f36e1ffd25aca90a86f403
tonussi <lptonussi@gmail.com>
2014-11-28 21:41:19 -0200

    Add constraints

---
+++ constraints.sql
@@ -0,0 +1,34 @@
+-- codcoord references coordenador (id)
+alter table tarefa add constraint codcoord_fkey foreign key (codcoord) references coordenador (id);
+-- codproj references projeto (id)
+alter table tarefa add constraint codproj_fkey foreign key (codproj) references projeto (id);
+-- codorc references orcamento (id)
+alter table orcamentosmateriais add constraint codorc_fkey foreign key (codorc) references orcamento (id);
+-- codmat references material (id)
+alter table orcamentosmateriais add constraint codmat_fkey foreign key (codmat) references material (id);
+-- codlocal references local (id)
+alter table alocacao add constraint codlocal_fkey foreign key (codlocal) references local (id);
+-- codhom references homologado (id)
+alter table alocacao add constraint codhom_fkey foreign key (codhom) references homologado (id);
+-- codass references assinatura (id)
+alter table orcamentosmateriais add constraint codass_fkey foreign key (codass) references assinatura (id);
+-- codaloc references alocacao (id)
+alter table orcamentosmateriais add constraint codaloc_fkey foreign key (codaloc) references alocacao (id);
+-- codmat references material (id)
+alter table orcamentosmateriais add constraint codmat_fkey foreign key (codmat) references material (id);
+-- codfun references funcionario (id)
+alter table assinatura add constraint codfun_fkey foreign key (codfun) references funcionario (id);
+-- codhomo references homologado (id)
+alter table assinatura add constraint codfun_fkey foreign key (codhomo) references homologado (id);
+-- codfun references funcionario (id)
+alter table participacaoprojeto add constraint codfun_fkey foreign key (codfun) references funcionario (id);
+-- codproj references projeto (id)
+alter table participacaoprojeto add constraint codproj_fkey foreign key (codproj) references projeto (id);
+-- codproj references projeto (id)
+alter table tarefa add constraint codproj_fkey foreign key (codproj) references projeto (id);
+-- codsubtar references subtarefas (id)
+alter table tarefa add constraint codsubtar_fkey foreign key (codsubtar) references tarefas (id);
+-- codprog references programador (id)
+alter table programadorlinguagem add constraint codprog_fkey foreign key (codprog) references programador (id, codfun);
+-- codling references linguagem (id)
+alter table programadorlinguagem add constraint codling_fkey foreign key (codling) references linguagem (id);
